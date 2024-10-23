73ed1dc747ad85751e4e32b5452a157b4708ed84
imihirjain <jmanav251@gmail.com>
2024-10-23 12:56:53 +0530

    make the dashboard private

---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+export default function PrivateRoute() {
+  const { currUser } = useSelector((state) => state.user);
+  return currUser ? <Outlet /> : <Navigate to="/signin" />;
+}
