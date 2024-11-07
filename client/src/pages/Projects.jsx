14d1396998d82aecb68d24f487244083d086e482
imihirjain <jmanav251@gmail.com>
2024-11-08 00:13:57 +0530

    color changes in about and project page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -5,14 +5,14 @@ export default function Projects() {
   return (
     <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
       <h1 className="text-3xl font-semibold">Projects</h1>
-      <p className="text-md text-gray-500">
+      <p className="text-md text-gray-500 dark:text-gray-400">
         At TechTonic, we believe in the power of hands-on learning. Our Projects
         section showcases a wide range of innovative tech projects that
         highlight the latest in web development, AI, cloud computing, data
         science, and beyond. Each project is designed to inspire, educate, and
         push the boundaries of what’s possible with technology.
       </p>
-      <p className="text-md text-gray-500">
+      <p className="text-md text-gray-500 dark:text-gray-400">
         Whether you’re a beginner looking for simple project ideas or an
         experienced developer aiming to explore advanced concepts, our Projects
         section has something for everyone. Each project includes detailed
@@ -22,28 +22,39 @@ export default function Projects() {
       <h1 className="font-semibold text-xl ">
         Explore Our Project Categories:
       </h1>
-      <ul className="list-disc flex flex-col gap-4 text-gray-500">
+      <ul className="list-disc flex flex-col gap-4 text-gray-500 dark:text-gray-400">
         <li>
-          <strong className="text-gray-700">Web Development:</strong> Build
-          dynamic, responsive websites using the latest frameworks and tools.
+          <strong className="text-gray-700 dark:text-gray-300">
+            Web Development:
+          </strong>{" "}
+          Build dynamic, responsive websites using the latest frameworks and
+          tools.
         </li>
         <li>
-          <strong className="text-gray-700">Data Science:</strong> Discover
-          projects that delve into machine learning, data visualization, and
-          analytics.
+          <strong className="text-gray-700 dark:text-gray-300">
+            Data Science:
+          </strong>{" "}
+          Discover projects that delve into machine learning, data
+          visualization, and analytics.
         </li>
         <li>
-          <strong className="text-gray-700">Cloud Computing:</strong> Learn
-          about scalable applications and cloud deployment strategies.
+          <strong className="text-gray-700 dark:text-gray-300">
+            Cloud Computing:
+          </strong>{" "}
+          Learn about scalable applications and cloud deployment strategies.
         </li>
         <li>
-          <strong className="text-gray-700">AI and Machine Learning: </strong>{" "}
+          <strong className="text-gray-700 dark:text-gray-300">
+            AI and Machine Learning:{" "}
+          </strong>{" "}
           Get hands-on experience with models, algorithms, and AI-driven
           applications.
         </li>
         <li>
-          <strong className="text-gray-700">Mobile Development:</strong> Create
-          apps for iOS and Android that enhance user experience and
+          <strong className="text-gray-700 dark:text-gray-300">
+            Mobile Development:
+          </strong>{" "}
+          Create apps for iOS and Android that enhance user experience and
           functionality.
         </li>
       </ul>
