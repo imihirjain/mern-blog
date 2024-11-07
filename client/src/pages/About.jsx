fc410026d4a43d90f5c9ec2ad26ec8a761891ed4
imihirjain <jmanav251@gmail.com>
2024-11-07 15:45:49 +0530

    complete about and project page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,41 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className="min-h-screen items-center justify-center flex ">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold my-7 text-center">
+            About TechTonic Blog
+          </h1>
+          <div className="flex flex-col gap-6 text-gray-500 text-md">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              amet, tenetur non voluptatem tempore distinctio incidunt voluptate
+              quae libero voluptas reiciendis pariatur, veniam sunt cumque? Quos
+              asperiores rerum aperiam illum dicta ipsa omnis, mollitia
+              architecto atque molestias quaerat? Molestiae quis atque porro
+              repudiandae ab dolorem fugiat neque ipsa praesentium numquam?
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
+              consequatur aut voluptatum accusantium numquam at, illum maxime
+              molestiae praesentium dolorum labore esse unde quo eveniet
+              reiciendis totam odio autem! Nihil omnis blanditiis consequatur ex
+              ipsa inventore iure officia, unde vitae rem. Ab saepe vero
+              doloremque in a. Veniam odio harum, veritatis ut vero iure eaque,
+              cupiditate iste eligendi ea similique!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+              nesciunt qui suscipit blanditiis minus, nam quidem omnis
+              repudiandae a vero quia, vel dicta, minima et optio reprehenderit
+              perspiciatis dolores excepturi.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
